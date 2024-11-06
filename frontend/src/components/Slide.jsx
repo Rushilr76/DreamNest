8643eaef7cdf61b2d34111c216aa9b198b0aea8e
Rushilr76 <rushilr112503@gmail.com>
2024-11-07 02:26:15 +0530

    Commit with both frontend and backend

---
+++ frontend/src/components/Slide.jsx
@@ -0,0 +1,13 @@
+import "../styles/Slide.scss"
+
+const Slide = () => {
+  return (
+    <div className="slide">
+      <h1>
+        Find Your Perfect Rental Home <br/> Browse Listings for Every Lifestyle and Budget
+      </h1>
+    </div>
+  );
+};
+
+export default Slide;
